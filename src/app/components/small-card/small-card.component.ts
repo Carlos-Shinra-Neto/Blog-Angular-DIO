b0f7b9e8f4f1701fb61ff9964f9454c27b239911
Carlos-Shinra-Neto <leosergi09@gmail.com>
2022-12-09 02:12:30 -0300

    Componentes Reativos

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,8 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SmallCardComponent implements OnInit {
 
-  constructor() { }
+  @Input()
+  photoCover:string = ""
 
+  @Input()
+  cardTitle:string = ""
+  
   ngOnInit(): void {
   }
 
